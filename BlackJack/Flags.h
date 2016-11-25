a279c7b823e14949389574d7b0bf0157d5301520
KobiHawk <kasaiaisu1@hotmail.com>
2016-11-25 01:22:42 -0600

    Additional file to hold debug mode flag.

---
+++ BlackJack/Flags.h
@@ -0,0 +1,10 @@
+#pragma once
+/*
+Flags.h
+This file contains the flag for debug mode to be turned on.
+This will cause Game.h to load a DebugDeck instead of a Deck,
+and Game.cpp not to shuffle before starting a new round.
+*/
+
+//The below line will turn on debug mode when executed.
+//#define _BLACKJACK_DEBUG
