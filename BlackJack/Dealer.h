3b34c0e4d8fe8481a2a52d00b857a21259eeab44
KobiHawk <kasaiaisu1@hotmail.com>
2016-11-27 05:00:34 -0600

    Forgot to add Dealer.h in last commit.

--- BlackJack/Dealer.h
+++ BlackJack/Dealer.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "Player.h"
 
+/*
+Dealer.h
+Child of Player
+
+The dealer only has one revealed card, and can only take very limited actions.
+*/
+
 class Dealer : public Player
 {
 public:
